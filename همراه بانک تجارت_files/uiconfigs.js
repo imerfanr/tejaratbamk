6a32f82a519448bd9df5c1a5a5366db2a38149d3
imerfanr <imerfanr@gmail.com>
2025-08-15 16:57:39 +0330

    Add files via upload

---
+++ همراه بانک تجارت_files/uiconfigs.js
@@ -0,0 +1,10 @@
+uiconfigs ={
+    "HELP_LINK_URL": "http://hbhelp.tejaratbank.ir/register.html",
+    "FORGOT_REFISTER_URL" :"https://my.tejaratbank.ir/web/ns/mainpage?execution=e2s1",
+    "INSTAGRAM_APP_LINK" : "instagram://user?username=tejaratbank",
+    "APARAT_LINK" :"https://www.aparat.com/tejaratbank",
+    "BASE_URL" :"https://mbt.tejaratbank.ir/api/",
+    "SERVER" :"MAIN"
+    
+   
+}
